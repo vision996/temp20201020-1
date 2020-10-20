f1b1d712eda0a31c2bfd17e3a6219487bd8c357d
vision996 <72532474+vision996@users.noreply.github.com>
2020-10-20 12:27:38 +0800

    小甲鱼小游戏
    第一次使用github上传文件

---
+++ p3游戏.py
@@ -0,0 +1,11 @@
+print("小甲鱼工作室=========')
+      temp=input("不妨猜想我心中的 想的是哪个数字“）
+                 guess=int(temp）
+                 if guess==8;
+                           print("你是我心中的的蛔虫吗？！“）
+                                 print(“猜中也没有用哦”）
+                                       else;
+                                       print(“猜错啦，我想的是8这个数字”）
+                                             print("游戏结束，不玩啦嘿嘿”）
+                   
+      
